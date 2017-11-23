e62c0ee266c6c2ace0eaf4bfb3028524defbe19f
Hoang Viet <davishoang96@gmail.com>
2017-11-24 00:45:21 +0700

    Clean up

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TestDLL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(13, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(296, 127);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Choose your Apps";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(205, 42);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(72, 17);
+            this.radioButton4.TabIndex = 11;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Calculator";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(205, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "IExplorer";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Wordpad";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Notepad";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 212);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ManagedInjector;
+
+namespace TestDLL
+{
+    public partial class Form1 : Form
+    {
+        public string filePath;
+        public string fileName;
+
+        public string dllPath;
+        public string dllName;
+        public static int chosenApp;
+
+        public static string notify;
+        
+
+
+        public enum _apps
+        {
+            notepad,
+            wordpad,
+            ieplore,
+            calc
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Visible = false;
+
+        }
+
+
+
+        
+
+
+        // INject button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var temp = chosenApp;
+            //MessageBox.Show(temp.ToString());
+            switch(temp)
+            {
+
+                case 0:
+                    doInject(_apps.notepad.ToString());
+                break;
+
+                case 1:
+                    doInject(_apps.wordpad.ToString());
+                break;
+                case 2:
+                    doInject(_apps.ieplore.ToString());
+                break;
+                case 3:
+                    doInject(_apps.calc.ToString());
+                break;
+
+            }
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            openApp(_apps.notepad.ToString());
+            chosenApp = (int)_apps.notepad;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            openApp(_apps.wordpad.ToString());
+            chosenApp = (int)_apps.wordpad;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            openApp(_apps.ieplore.ToString());
+            chosenApp = (int)_apps.ieplore;
+        }
+
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            openApp(_apps.calc.ToString());
+            chosenApp = (int)_apps.calc;
+        }
+
+
+        public void openApp(string application)
+        {
+
+            if(Process.GetProcessesByName(application).Length > 0)
+            {
+
+                foreach (Process running_app in Process.GetProcessesByName(application))
+                {
+                    running_app.Kill();
+                }
+            }
+
+                        
+
+            //fileName = Path.GetFileNameWithoutExtension(application);
+
+            new Thread (() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                /* run your code here */
+                Thread.Sleep(1000);
+                Process.Start(application);
+
+
+                //Thread.Sleep(1000);
+                //doInject(application);
+
+            }).Start();
+
+
+
+            //Thread.Sleep(2000);
+            //Process.Start(application);
+        }
+
+        public static void doInject(string apps)
+        {
+
+            do
+            {
+
+                var proc = Process.GetProcessesByName(apps).FirstOrDefault();
+                ManagedInjector.Injector.Launch(proc.MainWindowHandle,
+                    typeof(Loader).Assembly.Location,
+                    typeof(Loader).FullName, "Inject");
+            }
+            while (Process.GetProcessesByName(apps).Length < 0);
+        }
+    }
+
+    public class Loader
+    {
+        public static void Inject()
+        {
+            MessageBox.Show("Inject successful");
+        }
+    }
+}
