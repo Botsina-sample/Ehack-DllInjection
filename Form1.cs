c26183178c2a64b5ef4583fed04099d46c020049
Hoang Viet <davishoang96@gmail.com>
2017-11-24 10:31:10 +0700

    Clean up

--- Form1.cs
+++ Form1.cs
@@ -15,174 +15,179 @@ using ManagedInjector;
 
 namespace TestDLL
 {
-    public partial class Form1 : Form
-    {
-        public string filePath;
-        public string fileName;
+	public partial class Form1 : Form
+	{
+		public string filePath;
+		public string fileName;
 
-        public string dllPath;
-        public string dllName;
-        public static int chosenApp;
+		public string dllPath;
+		public string dllName;
+		public static int chosenApp;
 
-        public static string notify;
-        
-
-
-        public enum _apps
-        {
-            notepad,
-            wordpad,
-            ieplore,
-            calc
-        }
-
-        
-
-        public Form1()
-        {
-            InitializeComponent();
-            label2.Visible = false;
-
-        }
+		public static string notify;
+		
 
 
+		public enum _apps
+		{
+			notepad,
+			wordpad,
+			ieplore,
+			calc
+		}
 
-        
+		
+
+		public Form1()
+		{
+			InitializeComponent();
+			label2.Visible = false;
+
+		}
+
+		// INject button
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var temp = chosenApp;
+			//MessageBox.Show(temp.ToString());
+			switch(temp)
+			{
+
+				case 0:
+					doInject(_apps.notepad.ToString());
+				break;
+
+				case 1:
+					doInject(_apps.wordpad.ToString());
+				break;
+				case 2:
+					doInject(_apps.ieplore.ToString());
+				break;
+				case 3:
+					doInject(_apps.calc.ToString());
+				break;
+
+                default:
+                    MessageBox.Show("Not supported");
+                break;
+			}
+		}
 
 
-        // INject button
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var temp = chosenApp;
-            //MessageBox.Show(temp.ToString());
-            switch(temp)
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			openApp(_apps.notepad.ToString());
+			chosenApp = (int)_apps.notepad;
+		}
+
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		{
+			openApp(_apps.wordpad.ToString());
+			chosenApp = (int)_apps.wordpad;
+		}
+
+		private void radioButton3_CheckedChanged(object sender, EventArgs e)
+		{
+			openApp(_apps.ieplore.ToString());
+			chosenApp = (int)_apps.ieplore;
+		}
+
+
+		private void radioButton4_CheckedChanged(object sender, EventArgs e)
+		{
+			openApp(_apps.calc.ToString());
+			chosenApp = (int)_apps.calc;
+		}
+
+
+		public void openApp(string application)
+		{
+			if(Process.GetProcessesByName(application).Length > 0)
             {
+				foreach (Process running_app in Process.GetProcessesByName(application))
+				{
+					running_app.Kill();
+				}
+			}
+			//fileName = Path.GetFileNameWithoutExtension(application
+			new Thread (() =>
+			{
+				Thread.CurrentThread.IsBackground = true;
+				/* run your code here */
+				Thread.Sleep(1000);
+				Process.Start(application);
 
-                case 0:
-                    doInject(_apps.notepad.ToString());
-                break;
+				/*new thread to inject
+				Thread.Sleep(1000);
+				doInject(application); */
+                
+			}).Start();
 
-                case 1:
-                    doInject(_apps.wordpad.ToString());
-                break;
-                case 2:
-                    doInject(_apps.ieplore.ToString());
-                break;
-                case 3:
-                    doInject(_apps.calc.ToString());
-                break;
+		}
 
-            }
-        }
+		public static void doInject(string apps)
+		{
 
+			do
+			{
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            openApp(_apps.notepad.ToString());
-            chosenApp = (int)_apps.notepad;
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            openApp(_apps.wordpad.ToString());
-            chosenApp = (int)_apps.wordpad;
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            openApp(_apps.ieplore.ToString());
-            chosenApp = (int)_apps.ieplore;
-        }
+				var proc = Process.GetProcessesByName(apps).FirstOrDefault();
+				ManagedInjector.Injector.Launch(proc.MainWindowHandle,
+					typeof(Loader).Assembly.Location,
+					typeof(Loader).FullName, "Inject");
+			}
+			while (Process.GetProcessesByName(apps).Length < 0);
+		}
 
 
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-            openApp(_apps.calc.ToString());
-            chosenApp = (int)_apps.calc;
-        }
+		// broswe to app's location
+		private void button2_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog theDialog = new OpenFileDialog();
 
+			//Filter for EXE
+			theDialog.Title = "Open Exe File";
+			theDialog.Filter = "EXE files|*.exe";
+			theDialog.InitialDirectory = @"C:\";
 
-        public void openApp(string application)
-        {
+			if (theDialog.ShowDialog() == DialogResult.OK)
+			{
+				var selectedFile = Path.GetFileNameWithoutExtension(theDialog.FileName);
+				textBox1.Text = theDialog.FileName;
 
-            if(Process.GetProcessesByName(application).Length > 0)
-            {
-
-                foreach (Process running_app in Process.GetProcessesByName(application))
+                if(selectedFile == "wordpad")         
                 {
-                    running_app.Kill();
+                    chosenApp = (int)_apps.wordpad;
+                    MessageBox.Show(chosenApp.ToString());
                 }
-            }
 
-                        
-
-            //fileName = Path.GetFileNameWithoutExtension(application);
-
-            new Thread (() =>
-            {
-                Thread.CurrentThread.IsBackground = true;
-                /* run your code here */
-                Thread.Sleep(1000);
-                Process.Start(application);
-
-
-                Thread.Sleep(1000);
-                doInject(application);
-
-            }).Start();
+                if(selectedFile == "iexplore")
+                {
+                    chosenApp = (int)_apps.ieplore;
+                    MessageBox.Show(chosenApp.ToString());
+                }
 
 
 
-            //Thread.Sleep(2000);
-            //Process.Start(application);
-        }
-
-        public static void doInject(string apps)
-        {
-
-            do
-            {
-
-                var proc = Process.GetProcessesByName(apps).FirstOrDefault();
-                ManagedInjector.Injector.Launch(proc.MainWindowHandle,
-                    typeof(Loader).Assembly.Location,
-                    typeof(Loader).FullName, "Inject");
-            }
-            while (Process.GetProcessesByName(apps).Length < 0);
-        }
+                //MessageBox.Show(selectedFile);   
+				openApp(selectedFile);
+			}
+		}
 
 
-        // broswe to app's location
-        private void button2_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog theDialog = new OpenFileDialog();
+		//reset button for injector
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Application.Restart();
+		}
+	}
 
-            //Filter for EXE
-            theDialog.Title = "Open Exe File";
-            theDialog.Filter = "EXE files|*.exe";
-            //theDialog.InitialDirectory = @"C:\";
+	public class Loader
+	{
+		public static void Inject()
+		{
+			MessageBox.Show("Inject successful");
+		}
 
-            if (theDialog.ShowDialog() == DialogResult.OK)
-            {
-                var selectedFile = Path.GetFileNameWithoutExtension(theDialog.FileName);
-                textBox1.Text = theDialog.FileName;
-                //MessageBox.Show(selectedFile);
-                Process.Start(selectedFile);
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Application.Restart();
-        }
-    }
-
-    public class Loader
-    {
-        public static void Inject()
-        {
-            MessageBox.Show("Inject successful");
-        }
-
-    } 
+	} 
 }
